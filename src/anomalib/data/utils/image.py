6acfb9ce34c93c3f5ca7e2157d318ede46c6f5fa
Kyle Ferguson <kferguson@mbp-kferguson.local>
2025-05-30 10:03:33 -0600

    add support for floating point tiff images

--- src/anomalib/data/utils/image.py
+++ src/anomalib/data/utils/image.py
@@ -350,8 +350,12 @@ def read_image(path: str | Path, as_tensor: bool = False) -> torch.Tensor | np.n
     Returns:
         image as numpy array
     """
-    image = Image.open(path).convert("RGB")
-    return to_dtype(to_image(image), torch.float32, scale=True) if as_tensor else np.array(image) / 255.0
+    if path.endswith('.tif'):
+        image = tiff.imread(path)
+        return to_image(image) if as_tensor else np.array(image)
+    else:
+        image = Image.open(path).convert("RGB")
+        return to_dtype(to_image(image), torch.float32, scale=True) if as_tensor else np.array(image) / 255.0
 
 
 def read_mask(path: str | Path, as_tensor: bool = False) -> torch.Tensor | np.ndarray:
